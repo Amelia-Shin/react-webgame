02b0ad02a2708e83ad3bb70fe0d73332b5ae47da
Amelia-Shin <shinheewon97@gmail.com>
2025-05-13 10:57:09 +0900

    끝말잇기 게임 추가

--- lecture/WordRelay.jsx
+++ lecture/WordRelay.jsx
@@ -2,12 +2,52 @@ const React = require('react');
 const { Component } = React;
 
 class WordRelay extends Component {
-    state = {   
-        text: 'Hello, Webpack!',
+    state = {  
+        word: '희원',
+        value: '',
+        result: '',
     };
 
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+            this.setState((prevState) => {
+                return {
+                    word: prevState.value,
+                    result: '딩동댕',
+                    value: '',
+                };
+            });
+        } else {
+            this.setState({
+                result: '땡',
+                value: '',
+            });
+        }
+        this.input.focus();
+    }
+
+    onChangeInput = (e) => {
+        this.setState({
+            value: e.currentTarget.value,
+        });
+    }   
+
+    input;
+    onRefInput = (c) => {
+        this.input = c;
+    }   
     render() {
-        return <h1>{this.state.text}</h1>;
+        return (
+            <>
+                <h1>{this.state.word}</h1>
+                <form onSubmit={this.onSubmitForm}>
+                    <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+                    <button>입력!</button>
+                </form>
+                <div>{this.state.result}</div>
+            </>
+        ); 
     }
 };
 
