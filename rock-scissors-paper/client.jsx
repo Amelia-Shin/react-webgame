3f75bd07962ad8829b8036ed4a6bc11a652c9ef5
Amelia-Shin <gmldnjs0402@naver.com>
2025-05-25 15:18:40 +0900

    가위바위보 Hooks 컴포넌트 작성

--- rock-scissors-paper/client.jsx
+++ rock-scissors-paper/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import RSP from './RSPClass.jsx';
+import RSP from './RSP.jsx';
 
 createRoot(document.querySelector('#root')).render(<RSP />); 
