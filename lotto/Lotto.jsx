95d314eebea8a2f860e2c55f421d579b0781a2a0
Amelia-Shin <gmldnjs0402@naver.com>
2025-05-28 01:58:15 +0900

    useMemo , useCallback 사용

--- lotto/Lotto.jsx
+++ lotto/Lotto.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useMemo, useCallback } from "react";
 import Ball from "./Ball";
 
 function getWinNumbers() {
@@ -10,11 +10,14 @@ function getWinNumbers() {
     }
     const bonusNumber = shuffle[shuffle.length - 1];
     const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+    console.log(winNumbers + ',' + bonusNumber);
     return [...winNumbers, bonusNumber];
 }
 
 const Lotto = () => {
-  const [winNumbers, setWinNumbers] = useState(getWinNumbers());
+  // 함수 실행한 값을 저장해두기 위해서는 useMemo 사용 
+  const lottoNumbers = useMemo(() => getWinNumbers(), []); // 2번째 인자가 바뀌지 않는 한, getWinNumbers()는 2번 실행되지 않음
+  const [winNumbers, setWinNumbers] = useState(lottoNumbers);
   const [winBalls, setWinBalls] = useState([]);
   const [bonus, setBonus] = useState(null);
   const [redo, setRedo] = useState(false);
@@ -40,13 +43,17 @@ const Lotto = () => {
   }, [timeouts.current]); // input이 빈 배열이면 componentDidMount와 동일
   // 배열에 요소가 있으면 componentDidMount(첫 렌더링때 실행)랑 componentDidUpdate(state가 바뀔때마다 실행) 둘 다 수행
 
-  const onClickRedo = () => {
+  // useCallback 은 함수를 기억하는 역할 (함수형 컴포넌트가 변경되어도, onClickRedo 함수가 재생성되지 않음)
+  // * 자식 컴포넌트에 함수를 props로 넘겨줄 때, useCallback을 사용하면 자식 컴포넌트가 불필요하게 리렌더링 되는 것을 방지할 수 있음
+  const onClickRedo = useCallback(() => {
+    console.log(winNumbers); // useCallback 인자 값이 빈 상태에서 debug 찍어보면, winNumbers 초기 값을 계속 찍음 (처음의 winNumbers 를 기억하고 있어서)
+    // 최신의 winNumbers 값을 가지고 오고 싶다면 useCallback 인자 값에도 winNumbers 를 넣어주면 됨.
     setWinNumbers(getWinNumbers());
     setWinBalls([]);
     setBonus(null);
     setRedo(false);
     timeouts.current = [];
-  };
+  }, [winNumbers]);
 
   return (
     <>
