3bed099b62b8828cdfdaf0e87500a7ca451d72f4
Amelia-Shin <gmldnjs0402@naver.com>
2025-05-28 01:33:25 +0900

    Lotto hooks 변경 (useEffect 사용)

--- lotto/client.jsx
+++ lotto/client.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
 import { createRoot } from 'react-dom/client';
 
-import Lotto from './LottoClass'; 
+import Lotto from './Lotto'; 
 
 createRoot(document.querySelector('#root')).render(<Lotto />);
