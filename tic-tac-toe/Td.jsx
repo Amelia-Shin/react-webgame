5d6c0a08fea464e2c60d632f327d99907e7ce742
Amelia-Shin <gmldnjs0402@naver.com>
2025-06-09 23:28:06 +0900

    action 만들어서 dispatch하기

--- tic-tac-toe/Td.jsx
+++ tic-tac-toe/Td.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { CLICK_CELL, CHANGE_TURN } from "./TicTacToe";
 
-const Td = () => {
+const Td = ({ rowIndex, cellIndex, dispatch, cellData }) => {
+    const onClickTd = useCallback(() => {
+        console.log(`Clicked cell at row ${rowIndex}, column ${cellIndex}`);
+        dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex });
+        dispatch({ type: CHANGE_TURN }); // 턴 변경
+    }, []);
     return (
-        <td></td>
+        <td onClick={onClickTd}>{cellData}</td>
     );
 }
 
--- tic-tac-toe/TicTacToe.jsx
+++ tic-tac-toe/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useReducer, useCallback } from 'react';
+import React, { useReducer, useCallback } from 'react';
 import Table from './Table';
 
 const initialState = {
@@ -7,7 +7,11 @@ const initialState = {
     tableData: [['', '', ''], ['', '', ''], ['', '', '']],
 };
 
-const SET_WINNER = 'SET_WINNER';
+export const SET_WINNER = 'SET_WINNER';
+export const CLICK_CELL = 'CLICK_CELL';
+export const CHANGE_TURN = 'CHANGE_TURN';
+
+// dispatch 가 실행될 때 reducer 함수 호출
 const reducer = (state, action) => {
     switch (action.type) {
         case SET_WINNER:
@@ -16,6 +20,19 @@ const reducer = (state, action) => {
                 ...state, // 기존 state를 복사
                 winner: action.winner, // 승자 설정 (state 얕은 복사)
             };
+        case CLICK_CELL:
+            const tableData = [...state.tableData];
+            tableData[action.row] = [...tableData[action.row]]; // 해당 행을 복사하여 새로운 배열 생성
+            tableData[action.row][action.cell] = state.turn; // 클릭한 셀에 현재 턴의 플레이어 표시
+            return {
+                ...state,
+                tableData, // 업데이트된 테이블 데이터
+            };
+        case CHANGE_TURN:
+            return {
+                ...state,
+                turn: state.turn === 'O' ? 'X' : 'O', // 턴을 O와 X로 번갈아가며 변경
+            };
     }
 };
 
@@ -23,12 +40,12 @@ const TicTacToe = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
     const onClickTable = useCallback(() => {
-        dispatch({ type: SET_WINNER, winner: '0' });
+        dispatch({ type: SET_WINNER, winner: 'O' }); // dispatch를 통해 action을 보냄
     }, []);
 
     return (
         <>
-            <Table onClick={onClickTable} tableData={state.tableData}/>
+            <Table onClick={onClickTable} tableData={state.tableData} dispatch={dispatch}/>
             {state.winner && <div>{state.winner}님의 승리</div>}
         </>
     )
