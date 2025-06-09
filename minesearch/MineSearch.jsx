0a7c093fabf15e4e9276ef6ab79fee7628c52501
Amelia-Shin <gmldnjs0402@naver.com>
2025-06-10 01:54:35 +0900

    지뢰찾기 게임 환경설정 셋팅

---
+++ minesearch/MineSearch.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MineSearch = () => {
+  return (
+    <div className="container">
+      <h1>Mine Search</h1>
+      <p>Welcome to the Mine Search application!</p>
+      <p>This is a placeholder for the mine search functionality.</p>
+      <p>More features will be added soon.</p>
+    </div>
+  );
+};
+
+export default MineSearch;
---
+++ minesearch/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import MineSearch from './MineSearch';
+
+createRoot(document.querySelector('#root')).render(<MineSearch />);
